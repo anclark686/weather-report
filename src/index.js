7af1ac00ce3123f3afa9d22d35444e2f89ccd9eb
Aisha <amdahir1@gmail.com>
2023-06-07 22:27:11 -0500

    temperature text color change added

--- src/index.js
+++ src/index.js
@@ -19,17 +19,35 @@ const updateLandscape = (tempValue) => {
     landscape.textContent = landscape_text;
 };
 
+const updateTextColor = (tempValue) => {
+    if (tempValue > 80) {
+        document.getElementById("tempValue").style.color = "red";
+    } else if (tempValue >= 70 && tempValue <= 79) {
+        document.getElementById("tempValue").style.color = "orange";
+    } else if (tempValue >= 60 && tempValue <= 69) {
+        document.getElementById("tempValue").style.color = "yellow";
+    } else if (tempValue >= 50 && tempValue <= 59) {
+        document.getElementById("tempValue").style.color = "green";
+    } else if (tempValue <= 49) {
+        document.getElementById("tempValue").style.color = "teal";
+    }
+};
+
 let count = 72;
 updateLandscape(count);
+updateTextColor(count);
+
 
 decreaseButton.addEventListener("click", () => {
     count--;
     tempValue.textContent = count;
     updateLandscape(count);
+    updateTextColor(count);
 });
 
 increaseButton.addEventListener("click", () => {
     count++;
     tempValue.textContent = count;
     updateLandscape(count);
+    updateTextColor(count);
 });
