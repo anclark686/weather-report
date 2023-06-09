3fd10d7acb8e14ace8933e205d16a5c12739548b
Aisha <amdahir1@gmail.com>
2023-06-08 19:46:21 -0500

    waves 4 and 6 done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const tempValue = document.getElementById("tempValue");
 const landscape = document.getElementById("landscape");
 const cityNameInput = document.getElementById('cityNameInput');
 const skySelect = document.getElementById('skySelect');
+const cityNameReset = document.getElementById("cityNameReset")
+const currentTempButton = document.getElementById("currentTempButton");
+const cityNameInputDefault = document.getElementById("cityNameInput").defaultValue = "Seattle";
+const headerCityNameDefault = document.getElementById('headerCityName').textContent = "Seattle";
 
 const updateLandscape = (tempValue) => {
     let landscape_text = "";
@@ -22,7 +26,7 @@ const updateLandscape = (tempValue) => {
 };
 
 const updateTextColor = (tempValue) => {
-    if (tempValue > 80) {
+    if (tempValue >= 80) {
         document.getElementById("tempValue").style.color = "red";
     } else if (tempValue >= 70 && tempValue <= 79) {
         document.getElementById("tempValue").style.color = "orange";
@@ -35,13 +39,55 @@ const updateTextColor = (tempValue) => {
     }
 };
 
+const findLatitudeAndLongitude = (query) => {
+    let latitude, longitude;
+    axios.get('http://localhost:5000/location',
+        {
+            params: {
+                q: query,
+                format: 'json'
+            }
+        })
+        .then((response) => {
+            latitude = response.data[0].lat;
+            longitude = response.data[0].lon;
+
+            findWeather(latitude, longitude);
+        })
+        .catch((error) => {
+            console.log('error in findLatitudeAndLongitude!');
+        });
+}
+
+const findWeather = (latitude, longitude) => {
+    axios.get('http://localhost:5000/weather',
+        {
+            params: {
+                format: 'json',
+                lat: latitude,
+                lon: longitude
+            }
+        })
+        .then((response) => {
+            const tempK = response.data.main.temp;
+            const tempF = (tempK - 273.15) * 9 / 5 + 32;
+            document.getElementById("tempValue").textContent = Math.floor(tempF);
+            updateLandscape(tempF);
+            updateTextColor(tempF);
+        })
+        .catch((error) => {
+            console.log('error in findLocation!');
+        });
+    return tempF
+}
+
 let count = 72;
 updateLandscape(count);
 updateTextColor(count);
 
 const changeCity = () => {
     const cityName = document.getElementById('cityNameInput').value;
-    const cityHeader= document.getElementById('headerCityName');
+    const cityHeader = document.getElementById('headerCityName');
 
     cityHeader.textContent = cityName[0].toUpperCase() + cityName.substring(1);
 };
@@ -69,8 +115,8 @@ const updateSky = () => {
             gardenContent.classList = "garden__content snowy";
             break;
     };
-  };
-  
+};
+
 updateSky();
 
 const registerEventHandlers = () => {
@@ -80,7 +126,7 @@ const registerEventHandlers = () => {
         updateLandscape(count);
         updateTextColor(count);
     });
-    
+
     increaseButton.addEventListener("click", () => {
         count++;
         tempValue.textContent = count;
@@ -91,6 +137,17 @@ const registerEventHandlers = () => {
     cityNameInput.addEventListener("input", changeCity);
 
     skySelect.addEventListener("change", updateSky);
+
+    currentTempButton.addEventListener("click", () => {
+        const cityName = cityNameInput.value;
+        tempF = findLatitudeAndLongitude(cityName)
+    });
+
+    cityNameReset.addEventListener("click", () => {
+        cityNameInput.value = cityNameInputDefault
+        document.getElementById('headerCityName').textContent = "Seattle";
+    });
 };
+
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
